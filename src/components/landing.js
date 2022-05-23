860442449762426a58d61b3f33b37f30beed713a
Parsa Karimzadeh <skarimzadeh99@gmail.com>
2022-05-23 19:48:59 +0430

    done

---
+++ src/components/landing.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react';
+import { getCoin } from '../services/api';
+import Loader from './Loader';
+import Coin from './Coin';
+import styles from './Landing.module.css';
+
+const Landing = () => {
+
+    const [coins, setCoins] = useState([]);
+    const [search, setSearch] = useState("");
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            const data = await getCoin();
+            console.log(data);
+            setCoins(data);
+        }
+
+        fetchAPI();
+    }, [])
+
+    const searchHandler = event => {
+        setSearch(event.target.value);
+    }
+
+    const searchCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()));
+
+    return (
+        <div>
+            <>
+                <input className={styles.input} type="text" placeholder="Search" value={search} onChange={searchHandler} />
+                {
+                    coins.length ? 
+                    <div className={styles.coinContainer}>
+                    {
+                        searchCoins.map(coin => <Coin key={coin.id}
+                             name={coin.name}
+                             image={coin.image}
+                             symbol={coin.symbol}
+                             price={coin.current_price}
+                             marketCap={coin.market_cap}
+                             priceChange={coin.price_change_percentage_24h} />)
+                    }
+                    </div> :
+                    <Loader />
+                }
+            </>
+        </div>
+    );
+};
+
+export default Landing;
